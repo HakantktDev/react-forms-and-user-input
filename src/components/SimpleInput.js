b392ed27d50f615e16f0a379eafffa5b42504c6e
Hakan Tokat <hakantokatdev@gmail.com>
2023-03-01 11:52:43 +0300

    providing-validation-feedback

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -3,6 +3,7 @@ import { useState, useRef } from "react";
 const SimpleInput = (props) => {
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
@@ -12,18 +13,22 @@ const SimpleInput = (props) => {
     event.preventDefault();
 
     if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
       return;
     }
+    setEnteredNameIsValid(true);
 
     //nameInputRef.current.value = ""; !not ideal manuplation DOM directly
     setEnteredName("");
   };
+  const nameInputClasses = enteredNameIsValid ? "" : "invalid";
 
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className="form-control">
+      <div className={`form-control ${nameInputClasses}`}>
         <label htmlFor="name">Your Name</label>
         <input type="text" id="name" onChange={nameInputChangeHandler} ref={nameInputRef} value={enteredName} />
+        {!enteredNameIsValid && <p className="error-text">Please enter a valid name</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
