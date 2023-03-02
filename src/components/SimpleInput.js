af30292415b7ffc3d25ca1467838aa70eb495a36
Hakan Tokat <hakantokatdev@gmail.com>
2023-03-02 14:52:34 +0300

    initial-commit

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -17,7 +17,7 @@ const SimpleInput = (props) => {
     valueChangeHandler: emailChangeHandler,
     inputBlurHandler: emailBlurHandler,
     reset: resetEmailInput,
-  } = useInput((value) => value.trim() !== "" && value.includes("@"));
+  } = useInput((value) => value.includes("@"));
 
   let formIsValid = false;
 
